d1477bdba13a75d494f8f3f94f10af82655dc5f8
Robert Mugattarov <mugattarov@gmail.com>
2019-10-02 14:13:20 -0700

    updates

--- src/main/java/rmugattarov/bootwithmodules/BootWithModulesApplication.java
+++ src/main/java/rmugattarov/bootwithmodules/BootWithModulesApplication.java
@@ -3,12 +3,15 @@ package rmugattarov.bootwithmodules;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class BootWithModulesApplication {
 
 	public static void main(String[] args) {
 		var msg = "Howdy!";
 		System.out.println(msg);
+		System.out.println(Arrays.toString(args));
 		SpringApplication.run(BootWithModulesApplication.class, args);
 	}
 
